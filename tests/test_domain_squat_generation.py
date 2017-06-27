be0e92c0e8a3def233dae5a21d36a0276bd60f86
fhightower <floyd.hightower27@gmail.com>
2017-06-26 21:20:39 -0300

    Updating tests for new setup

--- tests/test_domain_squat_generation.py
+++ tests/test_domain_squat_generation.py
@@ -16,53 +16,21 @@ from xn_twist import xn_twist
 
 
 @pytest.fixture
-def default_args():
+def args():
     """Return the character sets as a pytest fixture."""
-    default_args = argparse.Namespace(cyrillic_advanced=False,
-                                      cyrillic_complete=True,
-                                      cyrillic_simplified=False,
-                                      dns=False, domain='waffle.com',
-                                      greek_advanced=False,
-                                      greek_complete=False,
-                                      greek_simplified=False,
-                                      latin_advanced=False,
-                                      latin_complete=False,
-                                      latin_simplified=False)
+    default_args = argparse.Namespace(dns=False, domain='waffle.com')
 
     return default_args
 
 
-@pytest.fixture
-def default_empty_args():
-    """Return the character sets as a pytest fixture."""
-    default_empty_args = argparse.Namespace(cyrillic_advanced=False,
-                                            cyrillic_complete=False,
-                                            cyrillic_simplified=False,
-                                            dns=False, domain='waffle.com',
-                                            greek_advanced=False,
-                                            greek_complete=False,
-                                            greek_simplified=False,
-                                            latin_advanced=False,
-                                            latin_complete=False,
-                                            latin_simplified=False)
-
-    return default_empty_args
-
-
-def test_domain_only(default_empty_args):
-    """Test `get_spoofable_chars` function."""
-    spoofable_chars = xn_twist.get_spoofable_charset(default_empty_args)[0]
-    assert len(spoofable_chars.keys()) == 0
-
-
-def test_domain_squat_generation(default_args):
+def test_domain_squat_generation(args):
     """Test the domain squat creation process."""
-    spoofable_chars = xn_twist.get_spoofable_charset(default_args)[0]
+    spoofable_chars = xn_twist.get_spoofable_charset(args)[0]
     assert len(spoofable_chars.keys()) == 1
 
-    domain_name, tld = xn_twist.get_domain_details(default_args.domain)
+    domain_name, domain_suffix = xn_twist.get_domain_details(args.domain)
     assert domain_name == "waffle"
-    assert tld == "com"
+    assert domain_suffix == "com"
 
     count = 0
     spoofable_indices = list()
