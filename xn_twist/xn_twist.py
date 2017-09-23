f632dec16f00a43eca2e930656885d4bb5c6b0da
Floyd Hightower <floyd.hightower27@gmail.com>
2017-09-23 16:28:20 -0300

    Catching a no answer error from python dns

--- xn_twist/xn_twist.py
+++ xn_twist/xn_twist.py
@@ -98,7 +98,10 @@ class XNTwist(object):
             dns_results = dns.resolver.query(domain)
             dns_records = [ip.address for ip in dns_results]
         except dns.resolver.NXDOMAIN as e:
-            # the domain does not exist so dns resolutions will remain None
+            # the domain does not exist so dns resolutions remain empty
+            pass
+        except dns.resolver.NoAnswer as e:
+            # the resolver is not answering so dns resolutions remain empty
             pass
 
         return dns_records
